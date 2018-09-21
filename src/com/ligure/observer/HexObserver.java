e7d3a3f23a023d12b0ccc0e518e2ee6c1ca5caff
ligure <shengliguo1009@163.com>
2018-09-21 13:49:19 +0800

    Update HexObserver.java

--- src/com/ligure/observer/HexObserver.java
+++ src/com/ligure/observer/HexObserver.java
@@ -5,8 +5,8 @@ package com.ligure.observer;
  */
 public class HexObserver extends Observer {
     @Override
-    public void update(int state) {
-        this.state = state;
+    public void update(Subject subject) {
+        this.state = subject.getState();
         System.out.println("Hex String: " + Integer.toHexString(state));
     }
 }
